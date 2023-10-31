6527e99481e6ffeeb9012fa5f3d172a0c3fe08a9
Medidali <m.mohamedidali.com>
2023-10-31 10:42:49 -0700

    all tasks

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[].scan(/\[from:([+\w]*)\] \[to:([+\w]*)\] \[flags:(.*?)\]/).join(separator=",")
+puts ARGV[0].scan(/\[from:([+\w]*)\] \[to:([+\w]*)\] \[flags:(.*?)\]/).join(separator=",")
